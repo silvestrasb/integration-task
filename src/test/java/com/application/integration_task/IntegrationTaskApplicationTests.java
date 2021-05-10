8f265db1f546836fcc3d681ec51542276aa61a24
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-10 21:27:27 +0300

    Default Spring Boot test.

---
+++ src/test/java/com/application/integration_task/IntegrationTaskApplicationTests.java
@@ -0,0 +1,14 @@
+package com.application.integration_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
