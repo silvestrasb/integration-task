279c2090490fac67169d8b9863dd4ec8b56c679f
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-10 21:18:25 +0300

    Implemented Service layer.

---
+++ src/main/java/com/application/integration_task/service/BeneficiaryService.java
@@ -0,0 +1,16 @@
+package com.application.integration_task.service;
+
+import com.application.integration_task.entity.Beneficiary;
+
+import java.util.List;
+
+public interface BeneficiaryService {
+    List<Beneficiary> findAll();
+
+    Beneficiary findById(int id);
+
+    void deleteById(int id);
+
+    void save(Beneficiary beneficiary);
+
+}
