be4b2860ce4707fbf698da1f87ac772438bcb2c1
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-10 23:13:18 +0300

    Optimized imports.

--- src/main/java/com/application/integration_task/controller/BeneficiaryRestController.java
+++ src/main/java/com/application/integration_task/controller/BeneficiaryRestController.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.net.URI;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.UUID;
 
