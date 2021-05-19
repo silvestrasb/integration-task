e899d3174cc56537162efeccfdeac439661bd459
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-19 17:26:23 +0300

    Renamed QRCodeProvider to QRCodeTecItProvider and implemented QRCodeProvider methods.

--- src/main/java/com/application/integration_task/util/QRCodeTecItProvider.java
+++ src/main/java/com/application/integration_task/util/QRCodeTecItProvider.java
@@ -5,18 +5,22 @@ import com.application.integration_task.entity.Beneficiary;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class QRCodeLink {
+public class QRCodeTecItProvider implements QRCodeProvider<Beneficiary>{
 
     // by default using https://qrcode.tec-it.com QR code provider's API
     private String QRCodeProvider = "https://qrcode.tec-it.com/API/QRCode?data=%s%s%s";
     private char deliminator = ',';
 
-    public String generateQRCodeLink(Beneficiary beneficiary) {
+    @Override
+    public String getLink(Beneficiary beneficiary) {
         return String.format(QRCodeProvider,
                 URLEncoder.encode(beneficiary.getUniqueCode(), StandardCharsets.UTF_8),
                 deliminator,
                 URLEncoder.encode(beneficiary.getName(), StandardCharsets.UTF_8));
     }
 
-
+    @Override
+    public void setDelimiter(char delimiter) {
+        this.deliminator = delimiter;
+    }
 }
