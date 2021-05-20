180173debaf7eba594135db62420b07d503325d6
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-21 00:17:29 +0300

    Added spirng boot annotations.

--- src/main/java/com/application/integration_task/util/QRCodeTecItProvider.java
+++ src/main/java/com/application/integration_task/util/QRCodeTecItProvider.java
@@ -1,14 +1,16 @@
 package com.application.integration_task.util;
 
 import com.application.integration_task.entity.Beneficiary;
+import org.springframework.stereotype.Component;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+@Component
 public class QRCodeTecItProvider implements QRCodeProvider<Beneficiary> {
 
     // by default using https://qrcode.tec-it.com QR code provider's API
-    private String QRCodeProvider = "https://qrcode.tec-it.com/API/QRCode?data=%s%s%s";
+    private final String QRCodeProvider = "https://qrcode.tec-it.com/API/QRCode?data=%s%s%s";
     private char deliminator = ',';
 
     @Override
--- src/main/java/com/application/integration_task/util/QRProviderFactory.java
+++ src/main/java/com/application/integration_task/util/QRProviderFactory.java
@@ -1,15 +1,21 @@
 package com.application.integration_task.util;
 
 import com.application.integration_task.entity.Beneficiary;
+import org.springframework.stereotype.Component;
 
+@Component
 public class QRProviderFactory {
 
+    private final QRCodeTecItProvider qrCodeTecItProvider;
+
+    public QRProviderFactory(QRCodeTecItProvider qrCodeTecItProvider) {
+        this.qrCodeTecItProvider = qrCodeTecItProvider;
+    }
+
     public QRCodeProvider<Beneficiary> getProvider(String provider) {
-        switch (provider) {
-            case "qrcode.tec-it.com":
-                return new QRCodeTecItProvider();
-            default:
-                return null;
+        if ("qrcode.tec-it.com".equals(provider)) {
+            return qrCodeTecItProvider;
         }
+        return null;
     }
 }
