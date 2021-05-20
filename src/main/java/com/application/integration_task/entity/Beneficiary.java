19383a9a98e7a8649ad46471920efc94d6ef1080
Silvestras Bumblauskas <silvestras.bumblauskas@gmail.com>
2021-05-20 17:37:02 +0300

    Added Swagger annotatinos to Beneficiary class.

--- src/main/java/com/application/integration_task/entity/Beneficiary.java
+++ src/main/java/com/application/integration_task/entity/Beneficiary.java
@@ -1,5 +1,7 @@
 package com.application.integration_task.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,23 +14,29 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 
+// Swagger annotations
+@ApiModel(description = "Details about the beneficiary.")
+
 // Database annotations
 @Entity
 @Table(name = "beneficiary")
 public class Beneficiary {
 
+    @ApiModelProperty(notes = "The primary key of the beneficiary.")
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ApiModelProperty(notes = "The unique code of the beneficiary (Max length 36).")
     @Column(name = "uniq_code", unique = true)
     private String uniqueCode;
 
+    @ApiModelProperty(notes = "The beneficiary's name (Max length 40).")
     @Column(name = "name")
     private String name;
 
-    public Beneficiary(String name){
+    public Beneficiary(String name) {
         this.name = name;
         this.uniqueCode = UUID.randomUUID().toString();
     }
